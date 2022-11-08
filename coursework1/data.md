cdb4f0766b57acfa731b3b67bdc1f74da6de4140
JocelynS371 <j.shao@hotmail.com>
2022-11-08 12:09:35 +0000

    New sections in code and markdown

--- coursework1/data.md
+++ coursework1/data.md
@@ -1,9 +1,11 @@
 # Data preparation and understanding
+
 ## Importing Data
+
 My intial plan was to upload the dataset onto github, then import the csv into my code. However, the file size was too large and the file must be stored on cloud. Therefore, the data is stored on my personal google drive and the url is used to access the data.  
 
 Here the first challenage arised. The link obtained from the drive is a share link. It could not be used in the code, therefore, I looked up how to change a share link into a direct link. The process is written into a function as below:  
-```
+```python
 def direct_link(link):
     """transform sharelink from google drive into a direct link
     
@@ -20,3 +22,20 @@ def direct_link(link):
     return link
 def main():
 ```
+This function should work for any google derive sharing link.  
+
+##Inspecting Data
+
+I inspected the size of the data frame. The data size was,
+(365011, 8)
+(96173, 8)
+(719788, 8)
+(623956, 8)
+respectively. The dataset is too large right now. The first thing we need to do is look for and clean any missing data. Due to the data size, any miss data is omitted rather than replaced.
+
+Note that from here on I will be using only 1 data frame as an example in this mark down file, because the running time is increasing into minutes.
+
+The info method showed no null value in dataframe F, however, A isnull check is done as well to be sure. And null data is dropped. Although there was no null data in F, there was nulls in other dataframe.
+
+##Dropping unnnessary data
+Considering that ocean current change on seasonal or yearly timeframe rather than an hourly basis, I decided to keep only 1 data entry per day. Because the date is entered as serial date based 1,Jan,0000. I rounded the number to interger and drop every data with non unique date.
--- coursework1/data_prep.py
+++ coursework1/data_prep.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 def direct_link(link):
     """transform sharelink from google drive into a direct link
@@ -18,21 +19,50 @@ def main():
     # Loading data. Dataset is too large for git, therefore hosted on google drive. 
     # read.csv would not work with sharelink so the link need to be phased
     url_F_share='https://drive.google.com/file/d/1-K8-BE6iA2b9FlA7fyg9i5wwidhsVFmR/view?usp=share_link'
-    url_G1_share='https://drive.google.com/file/d/1tNn90Z1GpGBemK7_Aj_ILOtGOb6LZF6z/view?usp=share_link'
-    url_UK1_share='https://drive.google.com/file/d/1SibXF_igCgND_RUaSJiki9L311LSjZHD/view?usp=share_link'
-    url_UK2_share='https://drive.google.com/file/d/1C5ajK4VHf2dac_fNS_ffFWZ1YcrDgP5M/view?usp=share_link'
+    #url_G1_share='https://drive.google.com/file/d/1tNn90Z1GpGBemK7_Aj_ILOtGOb6LZF6z/view?usp=share_link'
+    #url_UK1_share='https://drive.google.com/file/d/1SibXF_igCgND_RUaSJiki9L311LSjZHD/view?usp=share_link'
+    #url_UK2_share='https://drive.google.com/file/d/1C5ajK4VHf2dac_fNS_ffFWZ1YcrDgP5M/view?usp=share_link'
     
     url_F=direct_link(url_F_share)
-    url_G1=direct_link(url_G1_share)
-    url_UK1=direct_link(url_UK1_share)
-    url_UK2=direct_link(url_UK2_share)
+    #url_G1=direct_link(url_G1_share)
+    #url_UK1=direct_link(url_UK1_share)
+    #url_UK2=direct_link(url_UK2_share)
         
     df_F=pd.read_csv(url_F)
-    df_G1=pd.read_csv(url_G1)
-    df_UK1=pd.read_csv(url_UK1)
-    df_UK2=pd.read_csv(url_UK2)
-    print(df_F.shape)
-    print(df_G1.shape)
-    print(df_UK1.shape)
-    print(df_UK2.shape)
+    #df_G1=pd.read_csv(url_G1)
+    #df_UK1=pd.read_csv(url_UK1)
+    #df_UK2=pd.read_csv(url_UK2)
+    #print("The dataframe size is now")
+    #print(df_F.shape)
+    #print(df_G1.shape)
+    #print(df_UK1.shape)
+    #print(df_UK2.shape)
+    
+    #print(df_F.info(verbose=True))
+    #nulls_F = df_F[df_F.isnull().any(axis=1)]
+    #df_F=df_F.drop(nulls_F.index)
+    #print(nulls_F)
+    #print(df_F.info(verbose=True))
+    
+    #print(df_G1.info(verbose=True))
+    #nulls_G1 = df_G1[df_G1.isnull().any(axis=1)]
+    #print(nulls_G1)
+    #df_G1=df_G1.drop(nulls_G1.index)
+    #print(df_G1.info(verbose=True))
+    
+    #print(df_UK1.info(verbose=True))
+    #nulls_UK1 = df_UK1[df_UK1.isnull().any(axis=1)]
+    #df_UK1=df_UK1.drop(nulls_UK1.index)
+    #print(nulls_UK1)
+    #print(df_UK1.info(verbose=True))
+    
+    #print(df_UK2.info(verbose=True))
+    #nulls_UK2 = df_UK2[df_UK2.isnull().any(axis=1)]
+    #df_UK2=df_UK2.drop(nulls_UK2.index)
+    #print(nulls_UK2)
+    #print(df_UK2.info(verbose=True))
+    
+   
+    
+    
 main()
