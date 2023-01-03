cee4dde91a35eb4fde77877e72a527e567666b8d
JocelynS371 <j.shao@hotmail.com>
2023-01-03 19:47:30 +0000

    Managed to import code as package

---
+++ coursework2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+name="comp0035-cw-testing",
+version="1.0.0",
+packages=["src"],
+package_dir={
+"src": "./coursework2/src",
+},
+include_package_data=True,
+zip_safe=False,
+install_requires=[],
+)
--- coursework2/test/test_shopping_basket.py
+++ coursework2/test/test_shopping_basket.py
@@ -1,4 +1,16 @@
-import shopping_basket
+import pytest
+from coursework2.src.shopping_basket import Basket
+from coursework2.src.shopping_basket import Item
+
+def test_add_item():
+    """ Given item entered is valid, 
+    When item is added to basket, 
+    Then ensure item is added to the list, 
+    and the new total is updated """
+    i1 = Item("Warburtons", "Toastie", "800g white sliced loaf", decimal.Decimal('1.52'))
+    b=Basket
+    b.add_item(i1)
+    assert 
 
 
 
