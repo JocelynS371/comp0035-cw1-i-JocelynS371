15aa2730e95100529841b1d6d50665e80986fb10
JocelynS371 <j.shao@hotmail.com>
2023-01-04 09:57:26 +0000

    imported decimal into coftest

--- coursework2/test/conftest.py
+++ coursework2/test/conftest.py
@@ -2,6 +2,9 @@ import pytest
 from coursework2.src.shopping_basket import Basket
 from coursework2.src.shopping_basket import Item
 
+import decimal
+from decimal import Decimal
+from reprlib import recursive_repr
 @pytest.fixture
 def i1():
     i1 = Item("Warburtons", "Toastie", "800g white sliced loaf", decimal.Decimal('1.52'))
