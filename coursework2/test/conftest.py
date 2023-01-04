2c4c4143f47b76d1a5088b45ecab1498c0410884
JocelynS371 <j.shao@hotmail.com>
2023-01-04 10:37:01 +0000

    added test

--- coursework2/test/conftest.py
+++ coursework2/test/conftest.py
@@ -2,26 +2,15 @@ import pytest
 from coursework2.src.shopping_basket import Basket
 from coursework2.src.shopping_basket import Item
 
-import decimal
-from decimal import Decimal
-from reprlib import recursive_repr
-
-#@pytest.fixture
-#def i1():
-    #i1 = Item("Warburtons", "Toastie", "800g white sliced loaf", decimal.Decimal('1.52'))
-    #yield i1
-#@pytest.fixture
-#def i2():
-#    i2 = Item("Flora", "Buttery", "Buttery spread", decimal.Decimal('0.89'))
-#    yield i2
-#@pytest.fixture
-#def b():
-#    
-#    yield b
 @pytest.fixture
-def setup():
+def i1():
     i1 = Item("Warburtons", "Toastie", "800g white sliced loaf", decimal.Decimal('1.52'))
+    yield i1
+@pytest.fixture
+def i2():
     i2 = Item("Flora", "Buttery", "Buttery spread", decimal.Decimal('0.89'))
+    yield i2
+@pytest.fixture
+def b():
     b=Basket()
-    yield i1,i2,b
-#    yield i2
+    yield b
--- coursework2/test/test_shopping_basket.py
+++ coursework2/test/test_shopping_basket.py
@@ -6,29 +6,39 @@ import decimal
 from decimal import Decimal
 from reprlib import recursive_repr
 
-def test_add_item(setup):
+def test_add_item(i1,i2,b):
     """ Given item entered is valid, 
     When item is added to basket, 
     Then ensure item is added to the list, 
     and the new total is updated """
     b.add_item(i1, 1)
-    empty=b.is_empty()
-    total=b.get_total_cost()
-    assert empty==False
-    print(empty)
-    assert total==decimal.Decimal('1.52')
-def test_reset(setup):
-    """As a customer, I want to check if the basket is empty, so that I can prepare for the next shopping trip."
+    assert b.is_empty==False
+    assert b.get_total_cost==decimal.Decimal('1.52')
+def test_remove_item(i1,i2,b):
+    """ Given item entered is valid, 
+    When item is removed to basket, 
+    Then ensure item is removed from the list, 
+    and the new total is updated """
+    b.update_item(i1, 10)
+    b.remove_item(i1,1)
+    assert b.get_total_cost==decimal.Decimal('1.52')*9
+    b.remove_item(i1)
+    assert b.get_total_cost==decimal.Decimal('1.52')*8
+    b.remove_item(i1,-1)
+    assert b.get_total_cost==decimal.Decimal('1.52')*7
+def test_reset(i1,i2,b):
+    """Given the basket is filled with item, 
+    When the basket is reset, 
+    That the basket becomes empty, 
+    and That the total cost is 0
     """
-    b.add_item(i1, 1)
-    empty=b.is_empty()
-    assert empty==False
+    b.update_item(i1,10)
+    assert b.is_empty()==False
     b.reset()
-    empty=b.is_empty()
-    assert empty==True
+    assert b.is_empty()==True
     assert b.get_total_cost()==decimal.Decimal('1.52')*0
-def test_error(setup):
-    with pytest.raises(ValueError,match="Quantity must be a positive number") as excinfo:
+def test_error(i1,i2,b):
+    with pytest.raises(ValueError,match="Quantity must be a positive number"):
         raise ValueError("Quantity must be a positive number")
-    with pytest.raises(ValueError,match="Quantity must be a positive number") as excinfo:
+    with pytest.raises(ValueError,match="Quantity must be a positive number"):
         b.add_item(i1,0)
