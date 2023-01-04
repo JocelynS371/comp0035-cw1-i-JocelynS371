c241df4c40b0f607812c9e9660ca0f08dfe74c23
JocelynS371 <j.shao@hotmail.com>
2023-01-04 13:59:43 +0000

    Linting

--- coursework2/test/test_shopping_basket.py
+++ coursework2/test/test_shopping_basket.py
@@ -1,34 +1,26 @@
 import pytest
-
 import decimal
 
-def test_add_item(i1,i2,b):
 
-
-    """ 
-        Given item entered is valid, 
-        When item is added to basket, 
-        Then ensure item is added to the list, 
+def test_add_item(i1, i2, b):
+    """
+        Given item entered is valid,
+        When item is added to basket,
+        Then ensure item is added to the list,
         and the new total is updated """
-
-
     b.add_item(i1, 1)
     assert b.is_empty() == False
     assert b.get_total_cost() ==decimal.Decimal('1.52')
     
     
-def test_remove_item(i1,i2,b):
-
-
-    """ 
-        Given item entered is valid, 
-        When item is removed to basket, 
-        Then ensure item is removed from the list, 
+def test_remove_item(i1, i2, b):
+    """
+        Given item entered is valid,
+        When item is removed to basket,
+        Then ensure item is removed from the list,
         and the new total is updated """
-
-
     b.update_item(i1, 10)
-    b.remove_item(i1,2)
+    b.remove_item(i1, 2)
     assert b.items[i1] == 10-2
     assert b.get_total_cost() ==decimal.Decimal('1.52')*8
     b.remove_item(i1)
@@ -37,33 +29,25 @@ def test_remove_item(i1,i2,b):
         b.items[i1] == 0
         
         
-def test_reset(i1,i2,b):
-
-
+def test_reset(i1, i2, b):
     """
-        Given the basket is filled with item, 
-        When the basket is reset, 
-        That the basket becomes empty, 
+        Given the basket is filled with item,
+        When the basket is reset,
+        That the basket becomes empty,
         and That the total cost is 0
     """
-
-
-    b.update_item(i1,10)
+    b.update_item(i1, 10)
     assert b.is_empty() == False
     b.reset()
     assert b.is_empty() == True
     assert b.get_total_cost() ==decimal.Decimal('1.52')*0
     
     
-def test_error(i1,i2,b):
-
-
+def test_error(i1, i2, b):
     """
-        Given item entered is valid, 
-        When an negative number is entered, 
-        That the basket returens an exception 
+        Given item entered is valid,
+        When an negative number is entered,
+        That the basket returens an exception
     """
-
-
     with pytest.raises(ValueError,match="Quantity must be a positive number"):
-        b.add_item(i1,-1)
+        b.add_item(i1, -1)
